bc7d90400f81630c15035d1ea1d2d408e5838adb
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-10-04 12:37:06 +0300

    make build

---
+++ index.dca2de75.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");t.appendChild(e.firstChild),e.prepend(t)}),e.addEventListener("click",function(e){"SPAN"===e.target.tagName&&(e.target.nextSibling.hidden=!e.target.nextSibling.hidden)});
+//# sourceMappingURL=index.dca2de75.js.map
